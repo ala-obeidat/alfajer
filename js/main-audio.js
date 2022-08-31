68a3f6dcdbb7dff80c8c5b3fa21b5729766ace81
Ala Obeidat <ala_work@hotmail.com>
2022-08-31 13:05:20 +0300

    update audio call

--- js/main-audio.js
+++ js/main-audio.js
@@ -1,113 +1,85 @@
 
-import { config } from './config.js';
 import {init,start,answer,end,mute} from './call.js'
 
 
- // HTML elements
+// HTML elements
 const webcamVideo = document.getElementById('webcamVideo');
 const remoteVideo = document.getElementById('remoteVideo');
 const muteAudioButton = document.getElementById('muteAudio');
+const muteVideoButton = document.getElementById('muteVideo');
 
 const muteAudioIcon = document.getElementById('muteAudioIcon');
+const muteVideoIcon = document.getElementById('muteVideoIcon');
 
 const hangupButton = document.getElementById('hangupButton');
 const shareButton = document.getElementById('shareButton');
 const copyButton = document.getElementById('copyButton');
-const speakerButton = document.getElementById('speaker');
 
-
+shareButton.style.display='none';
+   copyButton.style.display='none';
 // 1. Setup media sources
 await init(false,(localStream,remoteStream)=>{
-  console.log('Call initiated');
-  webcamVideo.srcObject = localStream;
-  remoteVideo.srcObject = remoteStream;
+ console.log('Call initiated');
+ webcamVideo.srcObject = localStream;
+ remoteVideo.srcObject = remoteStream;
 },()=>{shareButton.remove();copyButton.remove();});
 console.log('Getting call Id...');
 const urlParams = new URLSearchParams(window.location.search);
 let callId = urlParams.get('call-id');
 if(callId){
-  console.log('Call Id is:',callId);
-  await answer(callId,()=>{
-    console.log('Call Answered:',callId);
-  });
+ console.log('Call Id is:',callId);
+ await answer(callId,()=>{
+   console.log('Call Answered:',callId);
+ });
 }else{
-  console.log('This is new call');
-  callId=await start(()=>console.log('Call Started'));
-  console.log("Id=",callId);
+ console.log('This is new call');
+ callId=await start(()=>{
+   console.log('Call Started');
+   shareButton.style.display='inline-block';
+   copyButton.style.display='inline-block';
+ });
+ console.log("Id=",callId);
 }
 shareButton.onclick=()=>{
-  const url= window.location.origin+'/audio.html?call-id='+callId;
-  window.open('whatsapp://send?text='+url); 
+ const url= window.location.origin+'/video.html?call-id='+callId;
+ window.open('whatsapp://send?text='+url); 
 }
-
 copyButton.onclick=async()=>{
-    const url= window.location.origin+'/audio.html?call-id='+callId;
-    await navigator.clipboard.writeText(url); 
-    alert('تم نسخ الرابط');
-  }
-  window.addEventListener('beforeunload', ()=>{
-    hangupButton.click();
-  });
-hangupButton.onclick=async ()=>{ await end(callId,false,()=>{
-  window.location.href= window.location.origin+'/thank.html';
-});};
+   const url= window.location.origin+'/video.html?call-id='+callId;
+   await navigator.clipboard.writeText(url); 
+   alert('تم نسخ الرابط');
+ }
+
+ hangupButton.onclick=async ()=>{ await end(callId,()=>{
+   window.location.href= window.location.origin+'/thank.html';
+ });};
+window.addEventListener('beforeunload', ()=>{
+ hangupButton.click();
+});
 muteAudioButton.onclick=()=>
 { 
-    mute(true,(active)=>{
-        if(active)
-        {
-            muteAudioIcon.className='fas fa-microphone-slash';
-            muteAudioButton.className='btn_option';
-        }else
-        {
-        muteAudioIcon.className='fas fa-microphone';
-        muteAudioButton.className='btn_option active';  
-        }
-    });
-};  
-speakerButton.onclick = async ()=>{
-  let i=0;
-  if(speakerButton.className.indexOf('active')==-1)
-  {
-    i=1;
-    speakerButton.className='active';
-  }
-  else{
-    speakerButton.className='';
-  }
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  
-  const outAudioDevices = devices.filter(device => device.kind === 'audiooutput');
-  const audioDevices = devices.filter(device => device.kind === 'audioinput');
-  if (!audioDevices[i] || !outAudioDevices[i]){
-   console.log('No devices');
-    return;
-  }
-  const outDeviceId=outAudioDevices[2].deviceId;
-  const deviceId = audioDevices[i].deviceId;
-  config.localStream.getAudioTracks().forEach((track) => {
-    track.stop();
-  });
-  // config.remoteStream.getAudioTracks().forEach((track) => {
-  //   track.stop();
-  // });
-  
-  const constraints = {
-    audio: {deviceId: {exact: deviceId}}
-  }; 
-  console.log(outAudioDevices[i]);
-  await remoteVideo.setSinkId(outDeviceId).catch(handleError);
-  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).catch(handleError);
-  
-}
-
-
-function gotStream(stream) {
-  // Refresh button list in case labels have become available
-  return navigator.mediaDevices.enumerateDevices();
-}
-function handleError(error) {
-
-  console.log('navigator.MediaDevices.getUserMedia error: ', error);
-
-}
+   mute(true,(active)=>{
+       if(active)
+       {
+         muteAudioIcon.className='fas fa-microphone';
+         muteAudioButton.className='btn_option active';    
+       }else
+       {
+         muteAudioIcon.className='fas fa-microphone-slash';
+         muteAudioButton.className='btn_option';
+       }
+   });
+};
+muteVideoButton.onclick=()=>{ 
+   mute(false,(active)=>{
+       if(active)
+       {
+         muteVideoIcon.className='fas fa-video';
+         muteVideoButton.className='btn_option active';  
+       }else
+       {
+           muteVideoIcon.className='fas fa-video-slash';
+           muteVideoButton.className='btn_option';
+       }
+   });
+};
