5e6faf8f839e9f38a9cedd60f0495afcace655f9
Ala Obeidat <ala_work@hotmail.com>
2023-04-16 13:58:26 +0300

    Add new actions

--- js/config.js
+++ js/config.js
@@ -1,13 +1,21 @@
 
 const servers={
   iceServers: [
-    //Add stun servers
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
   ],
   iceCandidatePoolSize: 10,
 }
 const firebaseConfig = {
-  //Add firebase config
-  };
+  apiKey: "AIzaSyBG5vZ8KxQa0AFiojtIsJECFpnKp_dI5fc",
+  authDomain: "al-fajer-a8af0.firebaseapp.com",
+  projectId: "al-fajer-a8af0",
+  storageBucket: "al-fajer-a8af0.appspot.com",
+  messagingSenderId: "49883731089",
+  appId: "1:49883731089:web:4b7d921832b6183411db30",
+  measurementId: "G-F1KT0J96S5"
+};
  
   export  const config={
     production:true,
